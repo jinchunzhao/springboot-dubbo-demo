419f65ac82d19a4a6b545df5e4a8ae5577d7e335
jinchunzhao <459147801@qq.com>
2021-07-31 10:28:41 +0800

    spring boot集成dubbo实现微服务架构

---
+++ order-web/src/test/java/com/jy/order/OrderWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
